6f8dbaac974a457404961884366bdf70bb872dc4
Dhaval Maru <you@example.com>
2022-11-19 08:10:32 +0530

    SqrOfNumber

---
+++ SqrOfNumber.cs
@@ -0,0 +1,38 @@
+namespace Scaler
+{
+public class SqrOfNumber
+{
+    static long Sqr()
+    {
+        long input = Convert.ToInt32(Console.ReadLine());
+
+        long start = 1;
+        long end = input/2;
+        long result = 0;
+        while(start<=end)
+        {
+            long mid = (start+end)/2;
+            long sqr = mid*mid;
+            if(sqr == input)
+            {
+                return mid;
+            }
+            else if(sqr<input)
+            {
+                start = mid + 1;
+                result = -1;
+            }
+            else
+            {
+                end = mid-1;
+            }
+        }
+        return result;
+    }
+    public static void Main(string[] args)
+    {
+        long value = Sqr();
+        Console.WriteLine(value);
+    }
+}
+}
