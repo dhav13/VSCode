bc379b2909282873dd8b6988a9fc8e21b5b587b1
Dhaval Maru <you@example.com>
2022-11-20 07:35:53 +0530

    IsItPrime

---
+++ IsItPrime.cs
@@ -0,0 +1,53 @@
+// Q1.Is It Prime?
+// Problem Description
+// Given an integer A as input, you have to tell whether it is a prime number or not.
+// A prime number is a natural number greater than 1 which is divisible only by 1 and itself.
+// Problem Constraints
+// 1 <= A <= 10^6
+// Input Format
+// First and only line of the input contains a single integer A.
+// Output Format
+// Print YES if A is a prime, else print NO.
+// Example  Input
+// Input 1:
+//  3
+// Input 2:
+//  4
+// Example Output
+// Output 1:
+//  YES
+// Output 2:
+//  NO
+// Example Explanation
+// Explanation 1:
+//  3 is a prime number as it is only divisible by 1 and 3.
+// Explanation 2:
+//  4 is not a prime number as it is divisible by 2.
+
+namespace Scaler
+{
+    public class IsItPrime
+    {
+        static void isItPrime(int input)
+        {
+            bool result = true;
+            for (int i = 2; i < input; i++)
+            {
+                if (input % i == 0)
+                {
+                    result = false;
+                    break;
+                }
+            }
+            if (result)
+                Console.WriteLine("YES");
+            else
+                Console.WriteLine("NO");
+        }
+        public static void Main(string[] args)
+        {
+            int input = Convert.ToInt32(Console.ReadLine());
+            isItPrime(input);
+        }
+    }
+}
