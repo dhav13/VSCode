e679cb95f3cf92efd7549c77d6055e4532d12167
Dhaval Maru <you@example.com>
2022-11-25 06:56:11 +0530

    CountAtleast1GreaterThanItSelf

---
+++ CountAtLeast1GreaterThanItSelf.cs
@@ -0,0 +1,34 @@
+namespace Scaler
+{
+    public class CountAtLeast1GreaterThanItSelf
+    {
+        static void BrutForceWay(int[] input)
+        {
+            int count = 0;
+            for (int i = 0; i < input.Length; i++)
+            {
+                for (int j = 0; j < input.Length; j++)
+                {
+                    if (input[i] > input[j])
+                    {
+                        count++;
+                        break;
+                    }
+                }
+            }
+            Console.WriteLine(count);
+        }
+
+        static void optimizedWay(int[] input)
+        {
+
+        }
+        public static void Main(string[] args)
+        {
+            int[] input = { -3, 2, 5, 8, 6, 7, 8, 10, -2 };
+            BrutForceWay(input);
+
+            optimizedWay(input);
+        }
+    }
+}
